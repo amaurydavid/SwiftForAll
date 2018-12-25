6eb3f10712a283a257c6506d2d253dedd4f00bf8
DAVID Amaury <amaury@smartnsoft.com>
2018-12-25 20:02:32 +0100

    Color names are now converted to camelCase to match Swift convention.

--- src/swiftColors.js
+++ src/swiftColors.js
@@ -1,20 +1,22 @@
-module.exports = {
-  getColorsSwiftFileContent: function(structName,colors) {
-    //Loop on every color
-    var colorsCode = "";
-    for (var color of colors){
-      colorsCode += "\n\t" + getColorSwiftCode(color);
-    }
-
-    //Wrap colors code in an extension
-    return `extension UIColor {
-      struct ${structName} {
-        ${colorsCode}
-      }
-    }`
+function getColorsSwiftFileContent(structName,colors) {
+  //Loop on every color
+  var colorsCode = "";
+  for (var color of colors){
+    colorsCode += "\n\t" + getColorSwiftCode(color);
   }
+
+  //Wrap colors code in an extension
+  return `extension UIColor {
+    struct ${structName} {
+      ${colorsCode}
+    }
+  }`
 }
 
+module.exports = { getColorsSwiftFileContent };
+
+var camelCase = require('camel-case')
+
 function getColorSwiftCode(color) {
-  return `static let ${color.name} = UIColor(red: ${color.r}/255.0, green: ${color.g}/255.0, blue: ${color.b}/255.0, alpha: ${color.a})`
+  return `static let ${camelCase(color.name)} = UIColor(red: ${color.r}/255.0, green: ${color.g}/255.0, blue: ${color.b}/255.0, alpha: ${color.a})`
 }
